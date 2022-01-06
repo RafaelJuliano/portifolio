2de61d37c0f7631e90c0f03cc5b47a9c8b596f06
Rafael Juliano Ferreira <67478757+RafaelJuliano@users.noreply.github.com>
2022-01-06 20:45:17 -0300

    Test https CSS call

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
